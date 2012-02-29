4bd07c3cb892c03ca8a602b9e2b7820c66418646
Mihai Tomescu <matomesc@gmail.com>
2012-02-29 18:18:41 -0500

    runner

--- index.js
+++ index.js
@@ -11,10 +11,20 @@ var cp = require('child_process')
   , util = require('util')
   , colors = require('colors')
   , Seq = require('seq')
-  , findit = require('findit');
+  , findit = require('findit')
+  , optimist = require('optimist');
 
 // might change
-var API = ['cp', 'mkdir', 'mv', 'rm', 'lessc', 'hint', 'test'];
+var API = {
+  cp: 'cp'
+, mkdir: 'mkdir'
+, mv: 'mv'
+, rm: 'rm'
+, lessc: './node_modules/less/bin/lessc'
+, hint: './node_modules/jshint/bin/hint'
+, test: './node_modules/mocha/bin/mocha'
+}
+// var API = ['cp', 'mkdir', 'mv', 'rm', 'lessc', 'hint', 'test'];
 
 var version = exports.version = require('./package.json').version;
 
@@ -80,35 +90,39 @@ merge(Task.prototype, buildProto(), {
 
 function buildProto() {
   var proto = {};
-  API.forEach(function (name) {
+  var execOpts = { cwd: process.cwd() };
+
+  Object.keys(API).forEach(function (name) {
+    var fullName = API[name];
     proto[name] = function (fn) {
       var self = this;
       var wrapper = function (cb) {
         if (typeof fn === 'string') {
-          cp.exec([name, fn].join(' '), cb);
+          cp.exec([fullName, fn].join(' '), execOpts, cb);
         } else if (typeof fn === 'function' && fn.length > 0) {
           // fn is an asynch function, pass done as a cb, then exec the result
           function done(err, result) {
-            if (Array.isArray(result)) {
-              result = [].concat.call([name], result);
-            }
             if (err) {
               return cb(err);
             }
-            cp.exec(result.join(' '), cb);
+            if (Array.isArray(result)) {
+              result = [].concat.call([fullName], result);
+            }
+            cp.exec(result.join(' '), execOpts, cb);
           }
           fn.call({}, done);
         } else if (typeof fn === 'function' && fn.length === 0) {
-          var result = fn.call({});
-          cp.exec(result.join(' '), cb);
+          var result = [].concat.call([fullName], fn.call({}));
+          cp.exec(result.join(' '), execOpts, cb);
         } else if (Array.isArray(fn)) {
-          var result = [].concat.call([name], result);
-          cp.exec(result.join(' '), cb);
+          var result = [].concat.call([fullName], execOpts, result);
+          cp.exec(result.join(' '), execOpts, cb);
         }
       }
       this.addSubtask(wrapper, true);
     };
   });
+
   return proto;
 }
 
@@ -116,19 +130,40 @@ function buildProto() {
 // Subtask
 //
 
-function Subtask(fn, async) {
+function Subtask(fn, async, isExec, cmd) {
   this.async = async;
   this.fn = fn;
+  this.isExec = isExec || false;
+  this.cmd = cmd || false;
 }
 
 //
 // Runner
 //
 
-function Runner() {}
+function Runner() {
+  this.tasks = {};
+}
+
+Runner.prototype = {
+  createTask: function (name, description) {
+    this.tasks[name] = new Task(name, description);
+    return this.tasks[name];
+  },
+  listTasks: function () {
+    var self = this;
+    var lines = Object.keys(this.tasks).map(function (name) {
+      var line = name;
+      while (line.length < 20) { line += ' '; }
+      line += self.tasks[name] ? self.tasks[name].desc : 'no desc';
+      return line;
+    });
+    console.log(lines.join('\n'));
+  }
+};
 
 //
-// cli
+// Cli
 //
 
 function Cli(cwd) {
@@ -140,16 +175,39 @@ function Cli(cwd) {
 
 Cli.prototype = {
   start: function () {
+    console.log("tax@%s", this.version)
     var path = this.findFile();
+
     if (!path) {
       this.fail([
-        "tax@%s - can't find tax.js"
+        "can't find tax.js"
       , "looked in the following directories:\n%s"].join('\n')
-      , this.version
       , this.searchedDirs.join('\n'));
     }
-    console.log(path);
-    console.log(this.searchedDirs);
+
+    var runner = new Runner();
+
+    // parse process.argv
+    var argv = optimist
+      .alias('l', 'list')
+      .argv;
+    var taskArgv = optimist.parse([].slice.call(process.argv, 3));
+
+    runner.createTask('hey', 'ho');
+    runner.createTask('yo', 'son');
+    // console.log(process.argv);
+    // console.log('argv: %j', argv);
+    // console.log('taskArgv: %j', taskArgv);
+
+    if (argv.list && argv._.length === 0) {
+      return runner.listTasks();
+    } else if (argv._.length > 0) {
+      var taskName = argv._[0];
+      console.log('run %s', taskName);
+    }
+
+    // console.log(path);
+    // console.log(this.searchedDirs);
   },
   findFile: function (_path) {
     var self = this;
